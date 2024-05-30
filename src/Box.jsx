096e7750d8e0d8fb15634f20a797b7e4a7cf5c61
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-05-30 15:32:28 -0400

    Updated doc string and props

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,21 +1,27 @@
 /** Render a box element
  *
- * Props: { height, width, backgroundColor}, deleteBox
+ * Props:
+ *  height
+ *  width
+ *  backgroundColor
+ *  deleteBox
+ *
+ * BoxList -> NewBoxForm -> Box
  */
 
-function Box ({ height, width, backgroundColor }, deleteBox) {
+function Box({ height, width, backgroundColor, deleteBox }) {
   const style = {
     width: width,
     height: height,
     backgroundColor: backgroundColor
-  }
+  };
 
   return (
     <div>
       <div style={style} ></div>
       <button onClick={deleteBox}></button>
     </div>
-  )
+  );
 }
 
 export default Box;
