b4b29798b567890dae7b5995da2040e609719120
Jacob <jacobrashid404@gmail.com>
2024-05-30 12:05:04 -0700

    Box component

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,5 +1,21 @@
-function Box () {
+/** Render a box element
+ *
+ * Props: { height, width, backgroundColor}, deleteBox
+ */
 
+function Box ({ height, width, backgroundColor }, deleteBox) {
+  const style = {
+    width: width,
+    height: height,
+    backgroundColor: backgroundColor
+  }
+
+  return (
+    <div>
+      <div style={style} ></div>
+      <button onClick={deleteBox}></button>
+    </div>
+  )
 }
 
 export default Box;
