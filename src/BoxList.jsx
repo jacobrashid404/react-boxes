4fea9a26e96b764959b9ebcee6bf745b097511f2
Jacob <jacobrashid404@gmail.com>
2024-05-30 12:05:14 -0700

    BoxList component

--- src/BoxList.jsx
+++ src/BoxList.jsx
@@ -1,5 +1,33 @@
-function BoxList() {
+import { useState } from "react";
+import NewBoxForm from "./NewBoxForm.jsx";
+import { v4 as uuid} from 'uuid';
 
+/** List of box components
+ *
+ *  State:
+ *    - boxes: array of Box components [Box, ...]
+ *
+ *  BoxList -> NewBoxForm -> Box -> BoxList
+ *  BoxList -> Box
+ */
+
+function BoxList() {
+  const [boxes, setBoxes] = useState([]);
+
+  function renderBoxes() {
+    return (
+      <ul>
+        {boxes.map(box => (
+          <li>{box}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  function addBox(box) {
+    let newBox = { ...boxes, id: uuid() };
+    setBoxes(boxes => [...boxes, newBox]);
+  }
 }
 
 export default BoxList;
