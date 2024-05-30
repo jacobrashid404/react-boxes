b2cd8e72e685902cb3d33ab5567907546267e557
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-05-30 15:32:52 -0400

    Imported Box and added deleteBox function.

--- src/BoxList.jsx
+++ src/BoxList.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import NewBoxForm from "./NewBoxForm.jsx";
-import { v4 as uuid} from 'uuid';
+import Box from "./Box.jsx";
+import { v4 as uuid } from 'uuid';
 
 /** List of box components
  *
  *  State:
- *    - boxes: array of Box components [Box, ...]
+ *    - boxes: array of Box props [ {width, height, bgColor}, ...]
  *
  *  BoxList -> NewBoxForm -> Box -> BoxList
  *  BoxList -> Box
@@ -18,7 +19,8 @@ function BoxList() {
     return (
       <ul>
         {boxes.map(box => (
-          <li>{box}</li>
+          <li><Box width={box.width} height={box.height} bgColor={box.bgColor} deleteBox={deleteBox} />
+          </li>
         ))}
       </ul>
     );
@@ -28,6 +30,18 @@ function BoxList() {
     let newBox = { ...boxes, id: uuid() };
     setBoxes(boxes => [...boxes, newBox]);
   }
+
+  function deleteBox(boxToDelete) {
+    let updatedBoxList = boxes.filter(box => box.id !== boxToDelete.id);
+    setBoxes(boxes => [...updatedBoxList]);
+  }
+
+  return (
+    <div className="BoxList">
+      <NewBoxForm addBox={addBox} />
+      {renderBoxes()}
+    </div>
+  );
 }
 
 export default BoxList;
