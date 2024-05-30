31b3a95a543a85141001fef4392e280e76e18cde
Jacob <jacobrashid404@gmail.com>
2024-05-30 12:05:30 -0700

    newBoxForm component

--- src/NewBoxForm.jsx
+++ src/NewBoxForm.jsx
@@ -1,5 +1,16 @@
-function NewBoxForm () {
+/** Form for adding new box to page
+ *
+ * State:
+ *  -formData: { height, width, backgroundColor}, addBox
+ *
+ * BoxList -> NewBoxForm
+ */
 
+function NewBoxForm ({ addBox }) {
+  function handleSubmit(evt) {
+    evt.preventDefault();
+
+  }
 }
 
 export default NewBoxForm;
